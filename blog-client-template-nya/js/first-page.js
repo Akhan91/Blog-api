d607c133030cbb3b536091ef04cf0774c39b5afa
Abdul Muqeet <abdul.khan@live.se>
2020-12-12 15:26:10 +0100

    Get all posts done

---
+++ blog-client-template-nya/js/first-page.js
@@ -0,0 +1,40 @@
+window.onload = function() { // Waits for the HTML document to completely load, before running the function
+    fetchAllPosts();
+}
+
+// Get all posts, First page
+async function fetchAllPosts() {
+    
+    try{
+        let response = await fetch('http://localhost:3000/posts');
+        let data = await response.json();
+        let bottomBorder = document.getElementsByClassName('bottom-border');
+        
+        let postsHTML = '';
+        for (posts of data) {
+            console.log(posts)
+            
+            // Title of posts + post content
+            postsHTML +=`
+            <br>
+            <h2>${posts.title}</h2>
+            <br>
+            <i>${posts.content}</i>
+            <br>`;
+
+
+            // Author + Date
+            let postsDate = new Date(posts.date);
+            postsHTML += `
+            <br>
+            <p><span>Author: ${posts.author}
+            <br>${postsDate.getFullYear()}-${postsDate.getMonth()}-${postsDate.getDate()}</span></p>
+            <br>
+            <span class = "bottom-border"></h2>`; // Dotted line element from CSS
+        }
+        document.getElementById('post-list').innerHTML = postsHTML;
+        
+    }catch (message) {
+        throw new Error(message);
+    }
+}
