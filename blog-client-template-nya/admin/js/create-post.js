de03cebe498330446466c36b8b37d48dad0f138d
Abdul Muqeet <abdul.khan@live.se>
2020-12-18 21:29:32 +0100

    Tags done and working

--- blog-client-template-nya/admin/js/create-post.js
+++ blog-client-template-nya/admin/js/create-post.js
@@ -1,27 +1,16 @@
 let form    = document.getElementById("create-post-form");
 form.addEventListener('submit', createPost);
-
+document.getElementById('title-textarea')
 
 async function createPost(e) {
     e.preventDefault();
-
-
     let formData = new FormData(this); // Easier to do this than e.g. '(document.getElementById('content-textarea').value)'
-
-    //This is the same as console.log(document.getElementById('content-textarea').value)
-    console.log('you ' + 'typed: ' + formData.get('content'));
-    
-
     let object = {
         content: formData.get('content'), // When using formData get the element by 'name' and not by 'id or class'.
         author:  document.getElementById('author-textarea').value,
         title:   document.getElementById('title-textarea').value,
-        tags:    document.getElementById('select-tags').value
+        tags:    $('#select-tags').val()
     }
-
-    // Shows the title, author, content
-    console.log(object);
-
     try {
         await fetch('http://localhost:3000/posts/', {
             method: 'POST',
